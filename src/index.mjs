dabc421960281c19ccdd127ff5d41efbd42fb50e
Kerim Imamović <imamovic.kerim@gmail.com>
2025-06-30 16:32:49 +0200

    Query params

--- src/index.mjs
+++ src/index.mjs
@@ -8,20 +8,31 @@ const mockUsers = [
   { id: 1, username: 'james', displayName: 'James' },
   { id: 2, username: 'john ', displayName: 'John' },
   { id: 3, username: 'robert', displayName: 'Robert' },
+  { id: 4, username: 'anson', displayName: 'Anson' },
+  { id: 5, username: 'kerim', displayName: 'Kerim' },
 ];
 
 const mockProducts = [
   { id: 1, name: 'Product 1', price: 9.90 },
   { id: 2, name: 'Product 2', price: 10.90 },
   { id: 3, name: 'Product 3', price: 11.90 },
-]
+];
 
 app.get('/', (req, res) => {
   res.status(200).send({ msg: 'Hello World!' });
 });
 
 app.get('/api/users', (req, res) => {
-  res.send(mockUsers);
+  console.log(req.query);
+  const { query: { filter, value } } = req;
+
+  if (!filter && !value) return res.send(mockUsers);
+
+  if (filter && value) return res.send(
+    mockUsers.filter((user) => user[filter].includes(value))
+  );
+
+  return res.send(mockUsers);
 });
 
 app.get('/api/users/:id', (req, res) => {
