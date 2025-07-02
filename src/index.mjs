320b9a1f60b9a8981255d6a27dd42da33fed675c
Kerim Imamović <imamovic.kerim@gmail.com>
2025-07-02 11:07:33 +0200

    Cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes/index.mjs';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser('helloworld'));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
@@ -13,5 +15,9 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
+  res.cookie('hello', 'world', {
+    maxAge: 60000 * 60,
+    signed: true,
+  });
   res.status(200).send({ msg: 'Hello World!' });
 });
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -4,7 +4,17 @@ import { mockProducts } from '../utils/constants.mjs';
 const router = Router();
 
 router.get('/api/products', (req, res) => {
-  res.send(mockProducts);
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
+  console.log(req.signedCookies);
+
+  if (req.signedCookies.hello && req.signedCookies.hello === 'world') {
+    return res.send(mockProducts);
+  };
+
+  return res
+    .status(403)
+    .send({ msg: 'Sorry. You need the correct cookie.' });
 });
 
 export default router;
